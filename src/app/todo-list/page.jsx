73421df66e20ad5514c389b461e0cf566faf4eeb
ffuad13 <jhonatom99@gmail.com>
2025-07-23 21:48:44 +0800

    add reducer state

--- src/app/todo-list/page.jsx
+++ src/app/todo-list/page.jsx
@@ -5,47 +5,13 @@ import TodoInput from "@/components/todoComponents/TodoInput";
 import TodoList from "@/components/todoComponents/todoList";
 
 export default function Home() {
-  // const hasTodos = todos.length > 0;
-  // const isLanguageIndonesian = false
-
-  // let list
-  // if (!hasTodos) {
-  //   list =  <p className="text-center text-gray-500">Task is empty. Please create a task</p>;
-  // } else {
-  //   list =  <TodoList todos={todos} toggleDone={toggleDone} deleteTodo={deleteTodo} />;
-  // }
-
-  return (
+   return (
     <TodoProvider>
       <main className="flex min-h-screen flex-col items-center justify-center p-6 bg-gray-100">
         <div className="w-full max-w-md space-y-4">
           <h1 className="text-2xl font-bold text-center">My Todo List</h1>
           <TodoInput />
           <TodoList />
-
-          {/* 1. if-else */}
-          {/* {(() => {
-          if (!hasTodos) {
-            return <p className="text-center text-gray-500">Task is empty. Please create a task</p>;
-          } else {
-            return <TodoList todos={todos} toggleDone={toggleDone} deleteTodo={deleteTodo} />;
-          }
-        })()} */}
-
-          {/* 2. Ternary operator */}
-          {/* {hasTodos ? (
-            <TodoList />
-          ) : isLanguageIndonesian ? (
-            <p className="text-center text-gray-500">Task kosong, silahkan buat Task baru.</p>
-          ) : (
-            <p className="text-center text-gray-500">Task is empty. Please create a task</p>
-          )} */}
-
-          {/* 3. Logical AND (&&) */}
-          {/* {hasTodos && <TodoList todos={todos} toggleDone={toggleDone} deleteTodo={deleteTodo} />}
-        {!hasTodos && <p className="text-center text-gray-500">Task is empty. Please create a task</p>} */}
-
-          {/* {list} */}
         </div>
       </main>
     </TodoProvider>
